a1b63ba79fe7e29ad1972a80a6fa86f0939240a2
Muhammed <muhamme7d7@gmail.com>
2025-01-04 22:50:55 -0900

    form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import "./styles.css"
 
 export default function App(){
-  return "Hi"
+  return (
+    <form className ="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input type="text" id="item" />
+      </div>
+      <button className="btn">Add</button>
+    </form>
+  )
 }
 //10:00
