bb23f97090eb6596322106ce803d9124d69bfa58
Muhammed <muhamme7d7@gmail.com>
2025-01-04 23:53:21 -0900

    toggletodo

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,19 @@ export default function App(){
 
   setNewItem("")
   }
+
+function toggleTodo(id, completed){
+  setTodos(currentTodos=>{
+    return currentTodos.map(todo =>{
+      if(todo.id === id){
+        return { ...todo, completed}
+      }
+
+      return todo
+    })
+  })
+}
+
   return (
     <>
     <form onSubmit={handleSubmit} className ="new-item-form">
@@ -37,7 +50,8 @@ export default function App(){
         return( 
         <li key={todo.id}>
         <label>
-          <input type="checkbox" checked={todo.completed} />
+          <input type="checkbox" checked={todo.completed}
+          onChange={e => toggleTodo(todo.id, e.target.checked)} />
           {todo.title}
         </label>
         <button className="btn btn-danger">Delete</button>
