6424b98a7e769351a98820030ed0182237956c2b
Muhammed <muhamme7d7@gmail.com>
2025-01-04 22:59:36 -0900

    check list

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./styles.css"
 
 export default function App(){
   return (
+    <>
     <form className ="new-item-form">
       <div className="form-row">
         <label htmlFor="item">New Item</label>
@@ -9,6 +10,24 @@ export default function App(){
       </div>
       <button className="btn">Add</button>
     </form>
+    <h1 className="header">Todo List</h1>
+    <ul className="list">
+      <li>
+        <label>
+          <input type="checkbox" />
+          Item 1
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+      <li>
+        <label>
+          <input type="checkbox" />
+          Item 2
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+    </ul>
+    </>
   )
 }
 //10:00
